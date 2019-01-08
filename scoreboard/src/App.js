6e5d03d479497a4c27c8eaf1ae741bb5f383448a
awiles808 <awiles808@gmail.com>
2019-01-08 16:53:09 -0700

    Added app.js

--- scoreboard/src/App.js
+++ scoreboard/src/App.js
@@ -1,28 +1,128 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
+const Header = (props) => {
+    console.log(props)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+            <h1>{props.title}</h1>
+            <span className='stats'>Players: {props.totalPlayers}</span>
+
         </header>
-      </div>
     );
-  }
 }
+
+const Player = (props) => {
+    return (
+        <div className="player">
+            <span className= "player-name">
+            <button className="remove-player" onClick={ () => props.removePlayer(props.id)}
+            >✖</button>
+                {props.name}
+            </span>
+            <Counter  />
+        </div>
+    );
+}
+class Counter extends React.Component {
+    state = {
+        score:0
+    };
+    incrementScore = () => {
+        this.setState( prevState => ({
+            score: prevState.score +1
+        }));
+    }
+
+
+    decrementScore = () => {
+        this.setState( prevState => ({
+            score: prevState.score -1
+        }));
+        console.log("Hi from the decrementScore!!")
+    }
+
+
+
+    render() {
+        return (
+            <div className="counter">
+                <button className="counter-action decrement" onClick=
+                    {this.decrementScore}> -</button>
+                <span className="counter-score">{this.state.score}</span>
+                <button className="counter-action increment" onClick=
+                    {this.incrementScore}> +</button>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+
+    state = {
+        players: [
+            {
+                name: "Guil",
+                id:1
+            },
+            {
+                name: "Treasure",
+                id:2
+            },
+            {
+                name: "Ashley",
+                id:3
+            },
+            {
+                name: "James",
+                id:4
+            }
+
+        ]
+    };
+
+    handleRemovePlayer = (id) => {
+        this.setState( prevState => {
+            return {
+                players: prevState.players.filter( p => p.id !== id)
+            };
+        });
+    }
+
+    render(){
+        return (
+            <div className="scoreboard">
+                <Header
+                    title="Scoreboard"
+                    totalPlayers={this.state.players.length}
+                />
+
+
+                {/*Players list */}
+                {this.state.players.map( player =>
+                    <Player
+                        name={player.name}
+                        id={player.id}
+                        score={player.score}
+                        key={player.id.toString()}
+                        removePlayer={this.handleRemovePlayer}
+                    />
+                )}
+            </div>
+
+        );
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
+
+
+
+
+
+
 
 export default App;
