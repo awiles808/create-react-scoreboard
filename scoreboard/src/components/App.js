5b38f4dcd38ce92509b383afef1aabb69b7b92ab
awiles808 <awiles808@gmail.com>
2019-01-21 10:14:05 -0700

    Push test

--- scoreboard/src/components/App.js
+++ scoreboard/src/components/App.js
@@ -30,6 +30,14 @@ class App extends Component {
     ]
   };
 
+    handleScoreChange = (delta) => {
+        // this.setState( prevState => ({
+        //     score: prevState.score + 1
+        // }));
+        console.log(delta)
+    }
+
+
   handleRemovePlayer = (id) => {
     this.setState( prevState => {
       return {
@@ -52,7 +60,8 @@ class App extends Component {
             name={player.name}
             score={player.score}
             id={player.id}
-            key={player.id.toString()} 
+            key={player.id.toString()}
+            changeScore={this.handleScoreChange}
             removePlayer={this.handleRemovePlayer}           
           />
         )}
