dde8621be713859f47f6f61d509f3c72291fca50
awiles808 <awiles808@gmail.com>
2019-01-24 20:56:29 -0700

    Handle stopwatch

--- scoreboard/src/components/Stopwatch.js
+++ scoreboard/src/components/Stopwatch.js
@@ -1,13 +1,38 @@
 import React, {Component} from 'react';
 
 class Stopwatch extends Component {
+
+    state = {
+        isRunning: false
+    };
+
+    componentDidMount() {
+        this.intervalID = setInterval(() => this.tick(), 100);
+
+    }
+
+    tick = () => {
+        console.log('ticking....')
+
+    }
+
+    handleStopwatch = () => {
+        this.setState({
+        isRunning: !this.state.isRunning
+        });
+    }
+
+
     render() {
         return(
             <div className="stopwatch">
                  <h2>Stopwatch</h2>
                 <span className="stopwatch-time">0</span>
-                <button>Start</button>
+                <button onClick={ this.handleStopwatch  }>
+                    { this.state.isRunning ? 'Stop' : 'Start'}
+                </button>
                 <button>Reset</button>
+            </div>
         );
     }
 }
