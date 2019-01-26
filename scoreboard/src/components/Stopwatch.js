d8a4556e808081b96745e94dea11740f4681c437
awiles808 <awiles808@gmail.com>
2019-01-25 18:11:58 -0700

    PreviousTime and dateNow

--- scoreboard/src/components/Stopwatch.js
+++ scoreboard/src/components/Stopwatch.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 class Stopwatch extends Component {
 
     state = {
-        isRunning: false
+        isRunning: false,
+        elapsedTime: 0,
+        previousTime: 0
     };
 
     componentDidMount() {
@@ -12,24 +14,34 @@ class Stopwatch extends Component {
     }
 
     tick = () => {
-        console.log('ticking....')
+        if (this.state.isRunning){
+        const  now = Date.now();
+        this.setState({
+            previousTime:now,
+            elapsedTime:this.state.elapsedTime + (now -
+                this.state.previousTime)
+        })
 
+        }
     }
 
     handleStopwatch = () => {
         this.setState({
-        isRunning: !this.state.isRunning
+            isRunning: !this.state.isRunning
         });
+        if (!this.state.isRunning) {
+            this.setState({previousTime: Date.now() });
+        }
+
     }
 
-
     render() {
-        return(
+        return (
             <div className="stopwatch">
-                 <h2>Stopwatch</h2>
+                <h2>Stopwatch</h2>
                 <span className="stopwatch-time">0</span>
-                <button onClick={ this.handleStopwatch  }>
-                    { this.state.isRunning ? 'Stop' : 'Start'}
+                <button onClick={this.handleStopwatch}>
+                    {this.state.isRunning ? 'Stop' : 'Start'}
                 </button>
                 <button>Reset</button>
             </div>
