8cceb03c0555b3d13b858a5deffca641bdb4cf96
awiles808 <awiles808@gmail.com>
2019-02-16 10:01:06 -0700

    Provider and Consumer

--- scoreboard/src/components/App.js
+++ scoreboard/src/components/App.js
@@ -1,4 +1,5 @@
- import React, {Component} from 'react';
+ import React, { Component } from 'react';
+import { Provider } from './Context';
 import Header from './Header';
 import Player from './Player';
 import AddPlayerForm from './AddPlayerForm'
@@ -67,27 +68,29 @@ class App extends Component {
 
   render() {
     return (
-      <div className="scoreboard">
-        <Header 
-          title="Scoreboard" 
-          players={this.state.players}
-        />
-  
-        {/* Players list */}
-        {this.state.players.map( ( player, index) =>
-          <Player 
-            name={player.name}
-            score={player.score}
-            id={player.id}
-            key={player.id.toString()}
-            index={index}
-            changeScore={this.handleScoreChange}
-            removePlayer={this.handleRemovePlayer}           
-          />
-        )}
+        <Provider>
+          <div className="scoreboard">
+            <Header
+              title="Scoreboard"
+              players={this.state.players}
+            />
 
-        <AddPlayerForm addPlayer={this.handleAddPlayer}/>
-      </div>
+            {/* Players list */}
+            {this.state.players.map( ( player, index) =>
+              <Player
+                name={player.name}
+                score={player.score}
+                id={player.id}
+                key={player.id.toString()}
+                index={index}
+                changeScore={this.handleScoreChange}
+                removePlayer={this.handleRemovePlayer}
+              />
+            )}
+
+            <AddPlayerForm addPlayer={this.handleAddPlayer}/>
+          </div>
+        </Provider>
     );
   }
 }
