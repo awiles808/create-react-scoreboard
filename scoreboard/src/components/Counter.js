b2b9de1153a780dba16a9d3178436e8b46a6c127
awiles808 <awiles808@gmail.com>
2019-02-16 09:01:45 -0700

    Create context api

--- scoreboard/src/components/Counter.js
+++ scoreboard/src/components/Counter.js
@@ -6,9 +6,11 @@ const Counter =({index, score, changeScore}) => {
 
         return (
             <div className="counter">
-                <button className="counter-action decrement" onClick={() => changeScore(index,-1)}> - </button>
+                <button className="counter-action decrement" onClick={
+                    () => changeScore(index,-1)}> - </button>
                 <span className="counter-score">{ score }</span>
-                <button className="counter-action increment" onClick={() => changeScore(index,+1)}> + </button>
+                <button className="counter-action increment" onClick={
+                    () => changeScore(index,+1)}> + </button>
             </div>
         );
 
